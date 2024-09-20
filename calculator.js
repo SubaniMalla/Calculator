d5adf48d0aa84b600ee3d7b7a6c9dacedfd3288b
SubaniMalla <subanimalla1@gmail.com>
2024-09-20 16:47:12 +0545

    Add functions for simple math

---
+++ calculator.js
@@ -0,0 +1,16 @@
+let num1, num2, op;
+function add(x,y){
+return x+y;
+}
+
+function subtract(x,y){
+return x-y;
+}
+
+function multiply(x,y){
+return x*y;
+}
+
+function divide(x,y){
+return x/y;
+}
